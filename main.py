bb0a821db6b543e5de8c98f2ec3c3bd017ba84cb
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2022-02-03 17:38:54 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,66 +1,25 @@
-#init
-
-import os
-
+from flask import Flask, jsonify, render_template, request
+from buttons import *
 
 
+app = Flask(__name__)
+
+htmlsrc = open('index.html', 'r')
+htmlsrc = htmlsrc.read()
 
 
-def b1():
-  return os.system('src\\b1.exe')
+@app.route('/')
+def index():
+    return f'{htmlsrc}'
 
+@app.route('/button1on')
+def button1_event_on():
+    var = button1()
+    var.run()
+    return ''
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
+@app.route('/button1off')
+def button1_event_off():
+    var = button1()
+    var.exit()
+    return ''
